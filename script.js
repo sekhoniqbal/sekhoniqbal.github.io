83a1922af2c79a12e19272d18470ab358f7fcc9d
sekhoniqbal <42016178+sekhoniqbal@users.noreply.github.com>
2019-01-15 17:53:06 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+window.onclick = function(e){
+  console.log(e);
+  posx = e.pageX;
+  posy = e.pageY;
+  left = (posx-200)+"px"
+  top = (posy-200)+"px"
+  condiv = document.createElement("div");
+  condiv.style.position = "absolute";
+  condiv.style.left = posx+"px";
+  condiv.style.top = posy+"px";
+  document.body.append(condiv);
+  color = randomColor();
+  condiv.innerHTML = `
+  <div class="wave wave1" style="border:2px solid ${color};"></div>
+  <div class="wave wave2" style="border:2px solid ${color};"></div>
+  <div class="wave wave3" style="border:2px solid ${color};"></div>
+  <div class="wave wave4" style="border:2px solid ${color};"></div>
+`;
+setTimeout(function(condiv){condiv.remove();}, 12000, condiv);
+
+}
+
+function randomColor(){
+var r = Math.floor(Math.random()*256);
+var g = Math.floor(Math.random()*256);
+var b = Math.floor(Math.random()*256);
+return `rgb(${r},${g},${b})`;
+}
